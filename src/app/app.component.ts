38ce0c49b71320c1bba4fb5b41520637ef685d2e
mdansari123 <saddamansari52@hotmail.com>
2017-10-02 00:37:21 +0530

    done
    done

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angulartypescript.com - Angular 2 UI Framework - Components';
+}
